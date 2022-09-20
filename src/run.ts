eeed0b6345927178714e1f447ccb96dab40880a2
Hidetake Iwata <int128@gmail.com>
2022-09-20 15:16:50 +0900

    Initial commit (#2)
    * Initial commit
    
    * Fix README

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,52 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 type Inputs = {
-  name: string
+  tags: string[]
+  expectedRevision: string
+  timeoutSeconds: number
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+  const ok = await retry(() => checkIfDockerImageRevisionIsExpected(inputs), inputs.timeoutSeconds * 1000, 3000)
+  if (!ok) {
+    throw new Error(`timed out until the Docker image is available`)
+  }
+}
+
+export const retry = async (satisfied: () => Promise<boolean>, timeout: number, polling: number): Promise<boolean> => {
+  const startedAt = Date.now()
+  while ((await satisfied()) === false) {
+    await new Promise((resolve) => setTimeout(resolve, polling))
+    const elapsed = Date.now() - startedAt
+    if (elapsed > timeout) {
+      return false
+    }
+  }
+  return true
+}
+
+const checkIfDockerImageRevisionIsExpected = async (inputs: Inputs): Promise<boolean> => {
+  for (const tag of inputs.tags) {
+    const revision = await getDockerImageRevision(tag)
+    if (revision !== inputs.expectedRevision) {
+      return false
+    }
+  }
+  return true
+}
+
+const getDockerImageRevision = async (tag: string): Promise<string | undefined> => {
+  const code = await core.group(`Pulling ${tag}`, () => exec.exec('docker', ['pull', tag], { ignoreReturnCode: true }))
+  if (code !== 0) {
+    return
+  }
+  const output = await exec.getExecOutput('docker', [
+    'image',
+    'inspect',
+    '-f',
+    '{{index .Config.Labels "org.opencontainers.image.revision"}}',
+    tag,
+  ])
+  return output.stdout.trim()
 }
